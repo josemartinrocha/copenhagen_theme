bf9d7ef201cb4fd65f502464b27f0259107bda44
josemmartinrocha <jmmr@holacons.com>
2023-01-11 15:45:28 +0100

    master -> reduce size

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SubcomponentComponent } from './subcomponent/subcomponent/subcomponent.component';
-import { OthersubcomponentComponent } from './othersubcomponent/othersubcomponent/othersubcomponent.component';
+import { SubcomponentComponent } from './subcomponent/subcomponent.component';
+import { OthersubcomponentComponent } from './othersubcomponent/othersubcomponent.component';
 
 @NgModule({
   declarations: [
